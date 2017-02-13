0134aef893fde85cdf850b66055eca1636ccbe95
unknown <mark_arias@bradycorp.com>
2017-02-13 19:08:49 +0800

    updated indexcontroller to check if handle_display attribute already exist or not, if not recreate the handle_display attribute

--- app/code/local/Ecommistry/ProductList/controllers/IndexController.php
+++ app/code/local/Ecommistry/ProductList/controllers/IndexController.php
@@ -4,24 +4,40 @@ class Ecommistry_ProductList_IndexController extends Mage_Core_Controller_Front_
     public function indexAction()
     {
     	if(!Mage::getSingleton('customer/session')->isLoggedIn()){
-	        $this->_redirect('/');
+	        $this->_redirectUrl('/customer/account/login/');
 	    }else{
-	        $collection = Mage::getModel('catalog/product')->getCollection();
-			$collection->addAttributeToSelect('sku');			
-			$collection->addFieldToFilter( 'handle_display', array('eq'=>1));
-			$collection->getSelect()->limit(10);
+	    	$attr = Mage::getModel('catalog/resource_eav_attribute')->loadByCode('catalog_product','handle_display');
+			if (null!==$attr->getId()) {
+		        $collection = Mage::getModel('catalog/product')->getCollection();
+				$collection->addAttributeToSelect('sku');			
+				$collection->addFieldToFilter( 'handle_display', array('eq'=>1));
+				$collection->getSelect()->limit(10);
 
-			$productListCollection = array();
-			foreach ($collection as $product) {			    
-			    $productListCollection['products'][] = $product->getData('sku');
-			}
+				$productListCollection = array();
+				foreach ($collection as $product) {			    
+				    $productListCollection['products'][] = $product->getData('sku');
+				}
 
-			$productListCollection['slidermode'] = $this->getRequest()->getParam('mode');	
-					
-			$block = $this->getLayout()->createBlock('core/template');
-	        $block->setTemplate('productlist/productlistview.phtml');
-	        $block->setProductlist($productListCollection);	        
-	        echo $block->toHtml();
+				$productListCollection['slidermode'] = $this->getRequest()->getParam('mode');	
+						
+				$block = $this->getLayout()->createBlock('core/template');
+		        $block->setTemplate('productlist/productlistview.phtml');
+		        $block->setProductlist($productListCollection);	        
+		        echo $block->toHtml();
+	    	}else{
+	    		/*	if productlist_setup was present in core_resource and handle_display was not found in eav_attribute
+					we are deleting the productlist_setup in core_resource to reinstall the module
+	    		*/
+	    		$sql = "DELETE FROM core_resource WHERE code = 'productlist_setup'";			 
+			    $connection = Mage::getSingleton('core/resource')->getConnection('core_write');			 
+			    try {
+			        $connection->query($sql);
+			        //reload page to reinstall module
+			        $this->_redirectUrl('/productlist/index/');
+			    } catch (Exception $e) {
+			        echo $e->getMessage();
+			    }
+	    	}
 	    }
 		
     } 
