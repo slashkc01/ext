86d5234c3bc1244028f4e5e65189a7d4e0a121ee
unknown <mark_arias@bradycorp.com>
2017-02-13 16:57:42 +0800

    fix errors on the attribute

--- app/code/local/Ecommistry/ProductList/controllers/IndexController.php
+++ app/code/local/Ecommistry/ProductList/controllers/IndexController.php
@@ -7,12 +7,9 @@ class Ecommistry_ProductList_IndexController extends Mage_Core_Controller_Front_
 	        $this->_redirect('*/*/');
 	    }else{
 	        $collection = Mage::getModel('catalog/product')->getCollection();
-			$collection->addAttributeToSelect('sku');  
-			$collection->addAttributeToSelect('handle_display');
+			$collection->addAttributeToSelect('sku');
 			
-			$collection->addFieldToFilter(array(
-			    array('name'=>'handle_display','eq'=>'1'),       
-			));
+			$collection->addFieldToFilter( 'handle_display', array('eq'=>1));
 
 			foreach ($collection as $product) {			    
 			    var_dump($product->getData());
