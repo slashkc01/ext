cfdd2e18332ab2facd8a5dca4aa72d92abde9285
unknown <mark_arias@bradycorp.com>
2017-02-13 18:44:55 +0800

    check if attribute handle_display already exist

--- app/code/local/Ecommistry/ProductList/sql/productlist_setup/mysql4-install-1.0.0.php
+++ app/code/local/Ecommistry/ProductList/sql/productlist_setup/mysql4-install-1.0.0.php
@@ -1,25 +1,30 @@
 <?php
-$installer = $this;
-$installer->startSetup();
+$attr = Mage::getModel('catalog/resource_eav_attribute')->loadByCode('catalog_product','handle_display');
+if (null!==$attr->getId()) {     
+    //already exist    
+}else{
+    //do not exist    
+    $installer = $this;
+    $installer->startSetup();
 
-$installer->addAttribute('catalog_product', 'handle_display', array(
-    'group'             => 'Default',
-    'label'             => 'Handle Display',
-    'input' 			=> 'boolean',
-	'type' 				=> 'int',
-    'backend'           => '',
-    'frontend'          => '',
-    'visible'           => true,
-    'required'          => false,
-    'user_defined'      => true,
-    'searchable'        => false,
-    'filterable'        => true,
-    'comparable'        => false,
-    'visible_on_front'  => true,
-    'visible_in_advanced_search' => false,
-    'unique'            => false,
-    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-));
+    $installer->addAttribute('catalog_product', 'handle_display', array(
+        'group'             => 'Default',
+        'label'             => 'Handle Display',
+        'input' 			=> 'boolean',
+    	'type' 				=> 'int',
+        'backend'           => '',
+        'frontend'          => '',
+        'visible'           => true,
+        'required'          => false,
+        'user_defined'      => true,
+        'searchable'        => false,
+        'filterable'        => true,
+        'comparable'        => false,
+        'visible_on_front'  => true,
+        'visible_in_advanced_search' => false,
+        'unique'            => false,
+        'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    ));
 
-
-$installer->endSetup();
+    $installer->endSetup();
+}
